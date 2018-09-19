3bed75b81f64061d40718be5313fc886b34e8a6b
not-so-smart <43360094+not-so-smart@users.noreply.github.com>
2018-09-19 16:35:48 -0400

    Add preview.js

---
+++ commands/preview.js
@@ -0,0 +1,13 @@
+module.exports = {
+    name: 'preview',
+    displayName: 'Preview',
+    description: 'Shhhhhh!',
+    aliases: [],
+    usage: '',
+    args: false,
+    cooldown: 3,
+    accessLevel: 0,
+    async execute(message, args) {
+        // CODE HERE
+    }
+};
