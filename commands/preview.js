486edbd7b50ac899f3c646ecb387f8ad7b4ea01f
not-so-smart <43360094+not-so-smart@users.noreply.github.com>
2018-09-30 21:37:15 -0400

    Add preview.js to commands

--- commands/preview.js
+++ commands/preview.js
@@ -1,5 +1,5 @@
-const Discord = require('discord.js');
 const Canvas = require('canvas');
+const Discord = require('discord.js');
 const snekfetch = require('snekfetch');
 const { Users } = require('../dbObjects.js');
 const { CommunityRoles, SkillRoles } = require('../data/roles.json');
@@ -32,37 +32,50 @@ module.exports = {
                 return user;
             })
             .catch(error => console.error('Error finding or creating user:\n' + error));
-        
+
         const crId = findRole(targetMember, CommunityRoles);
         const srId = findRole(targetMember, SkillRoles);
         const communityRole = CommunityRoles[crId];
         const skillRole = SkillRoles[srId];
 
         const template = 'shit-template';
-
+        
+        Canvas.registerFont('C:/Users/nope/DaVinci/fonts/comic.ttf', {family: 'Comic Sans'});
         const canvas = Canvas.createCanvas(500, 360);
         const ctx = canvas.getContext('2d');
 
-        const background = await Canvas.loadImage(`../templates/${template}/background.jpg`);
+        const background = await Canvas.loadImage(`C:/Users/nope/DaVinci/templates/${template}/background.png`);
         ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
 
         const { body: buffer } = await snekfetch.get(targetMember.user.displayAvatarURL);
         const avatar = await Canvas.loadImage(buffer);
         ctx.drawImage(avatar, 37, 35, 64, 65);
 
-        ctx.font = '60px comic-sans';
+        ctx.font = '60px Comic Sans';
         ctx.fillStyle = '#000000';
-        ctx.fillText(targetMember.displayName, 218, 42);
+        ctx.textAlign = 'center';
+        ctx.fillText(targetMember.displayName, 175, 82);
 
-        const renderSR = await Canvas.loadImage(`../templates/${template}/skill/${skillRole.name}.png`);
-        ctx.drawImage(renderSR, 273, 164, 168, 43);
+        if (skillRole) {
+            const renderSR = await Canvas.loadImage(`C:/Users/nope/DaVinci/templates/${template}/skill/${skillRole.name}.png`);
+            ctx.drawImage(renderSR, 267, 160, 168, 43);
+        }
 
-        const renderCR = await Canvas.loadImage(`../templates/${template}/community/${communityRole.name}.png`);
-        ctx.drawImage(renderCR, 210, 256, 168, 43);
+        if (communityRole) {
+            const renderCR = await Canvas.loadImage(`C:/Users/nope/DaVinci/templates/${template}/community/${communityRole.name}.png`);
+            ctx.drawImage(renderCR, 207, 256, 168, 43);
+        }
+
+        if (target.heartCount) {                              // CHANGE THIS
+            ctx.font = '20px Comic Sans';
+            ctx.fillStyle = '#000000';
+            ctx.textAlign = 'center';
+            ctx.fillText(target.heartCount, 448, 328);
+        }
 
         const attachment = new Discord.Attachment(canvas.toBuffer(), 'profile.png');
-        channel.send(`Profile card for **${targetMember.displayName}**:`, attachment);
-        
+        message.channel.send(`Profile card for **${targetMember.displayName}**:`, attachment);
+
 
 
 
